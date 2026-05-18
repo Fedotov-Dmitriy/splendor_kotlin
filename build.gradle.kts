28e1813d1e6ca1e48d75e4db7c822c21462c9251
Fedotov-Dmitriy <qwertyyy20262@mail.ru>
2026-05-18 08:41:53 +0300

    Add SQLite JDBC dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.xerial:sqlite-jdbc:3.45.3.0")
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
